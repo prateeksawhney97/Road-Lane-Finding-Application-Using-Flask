1a46dac4aad7a9627bb8d879aee5c9226698d146
prateeksawhney97 <prateek.sawhney97@gmail.com>
2020-01-07 19:45:43 +0530

    performing cv2 operations

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import pandas as pd
 import numpy as np
 import os
 
+import pickle
+import cv2
+import glob
+
 IMAGE_FOLDER = 'static/'
+PROCESSED_FOLDER = 'processed/'
 #IMAGE_FOLDER = os.path.join('upload', 'images')
 
 
@@ -14,6 +19,8 @@ from sklearn.naive_bayes import MultinomialNB
 
 app = Flask(__name__)  
 app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
+app.config['PROCESSED_FOLDER'] = PROCESSED_FOLDER
+
 
 @app.route('/')  
 def upload():
@@ -23,13 +30,23 @@ def upload():
 def success():
 	if request.method == 'POST':
 		f = request.files['file']
+		#print(f)
+		#image=f
+		#hls = (np.float32(image), cv2.COLOR_RGB2HLS)
+		#f=hls
 		f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
 		full_filename = os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
+		#print(f)
+		#filepath = os.path.join(app.config['imgdir'], filename);
+		#file.save(filepath)
+		image_ext = cv2.imread(full_filename)
+		#image = full_filename
 		return render_template("success.html", name = f.filename, img = full_filename)
 
-@app.route("/predict", methods=['POST'])
+@app.route('/predict', methods = ['POST'])
 def predict():
-	return render_template("abc.html")
+	if request.method == 'POST':
+		return render_template("abc.html")
 
 
 if __name__ == '__main__':  
